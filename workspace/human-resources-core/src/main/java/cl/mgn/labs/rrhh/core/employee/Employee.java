d0f8c31f278c5df059dc935df426219f0d8fabbe
Juan Maldonado <juan.maldonado.leon@gmail.com>
2015-06-05 18:28:48 -0300

    Initial commit

---
+++ workspace/human-resources-core/src/main/java/cl/mgn/labs/rrhh/core/employee/Employee.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) MgnLabs 2010-2015
+ * Factoría de Desarrollo de Software
+ * Santiago de Chile, Region Metropolitana
+ */
+package cl.mgn.labs.rrhh.core.employee;
+
+import cl.mgn.labs.rrhh.core.person.Person;
+
+/**
+ * ---------------------------------------------------------------------------
+ * MGNlabs 20114 - 2015 
+ * Gestion de Recursos Humanos -
+ * @author Juan Francisco Maldonado León - ( juan.maldonado.leon@gmail.com )
+ * @date 22-05-2015
+ * Description : 
+ * ---------------------------------------------------------------------------
+ */
+public class Employee extends Person {
+
+	
+}
---
+++ workspace/human-resources-core/src/main/java/cl/mgn/labs/rrhh/core/person/Person.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) MgnLabs 2010-2015
+ * Factoría de Desarrollo de Software
+ * Santiago de Chile, Region Metropolitana
+ */
+package cl.mgn.labs.rrhh.core.person;
+
+import java.util.Date;
+
+/**
+ * ---------------------------------------------------------------------------
+ * MGNlabs 20114 - 2015 
+ * Gestion de Recursos Humanos -
+ * @author Juan Francisco Maldonado León - ( juan.maldonado.leon@gmail.com )
+ * @date 20-04-2015
+ * Description : 
+ * ---------------------------------------------------------------------------
+ */
+public class Person {
+	
+	private IdPerson idPerson;
+	private String name;
+	private String middleName;
+	private String lastName;
+	private Date birthday;
+	private Gender gender;
+
+}
